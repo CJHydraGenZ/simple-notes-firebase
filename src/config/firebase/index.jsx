2fab93166b7b3462ad568820f0d4258ef94d074b
CJHydraGenZ <cahyasaga321@gmail.com>
2019-11-04 11:14:08 +0800

    fstrutur folder

---
+++ src/config/firebase/index.jsx
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDTSUvlkc2bCDZqtO-4dri-s7AXB4NXNjc",
+    authDomain: "simple-notes-firebase-18d09.firebaseapp.com",
+    databaseURL: "https://simple-notes-firebase-18d09.firebaseio.com",
+    projectId: "simple-notes-firebase-18d09",
+    storageBucket: "simple-notes-firebase-18d09.appspot.com",
+    messagingSenderId: "595328341327",
+    appId: "1:595328341327:web:c1dafcfdf24e2e2774a13a",
+    measurementId: "G-Y6SWPNFDQ8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './container/pages/App/index';
 import * as serviceWorker from './serviceWorker';
+import firebase from './config/firebase'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log('config firebawe', firebase);
+
+
+ReactDOM.render( < App / > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
