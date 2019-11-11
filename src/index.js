c8c05e509305f76258360911bf5ce84196f76762
CJHydraGenZ <cahyasaga321@gmail.com>
2019-11-11 10:01:58 +0800

    preiter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './container/pages/App/index.jsx';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./container/pages/App/index.jsx";
+import * as serviceWorker from "./serviceWorker";
 // import firebase from './config/firebase'
-
 
 // console.log('config firebawe', firebase);
 
-
-
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
